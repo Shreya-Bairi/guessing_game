26c4549a580d540b7ee3b13142a0b01bac205fb8
Shreya-Bairi <78089584+Shreya-Bairi@users.noreply.github.com>
2022-09-24 16:28:35 +0530

    level 1
    added level 1 of the game.

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -26,4 +26,46 @@ int main()
     int secretnumber;
     secretnumber = (rand() % (100-1)+1);
     int guess;
+    
+    
+    //EASY
+    if (difficulty == 1)
+    {
+        cout << "You have 10 chances to find the secret number between 1 and 100.";
+        int chancesleft;
+        chancesleft = 10;
+
+        for (int i=0; i < 11; i++)
+        {
+            cout << "\nEnter a number: ";
+            cin >> guess;
+          
+            if (guess == secretnumber) {
+                cout << "Well Done! You Won! " << guess << " is the secret number" << endl;
+                break;
+            }
+            else {
+                cout << "You guessed it wrong.\n";
+                if (guess > secretnumber)
+                {
+                    cout << "Guess lower!\n";
+                }
+                else 
+                {
+                    cout << "Guess higher!\n";
+                }
+                chancesleft--;
+                cout << chancesleft << " chances left" << endl;
+                if (chancesleft == 0)
+                {
+                    cout << "You are out of chances!";
+                    cout << "\nThe secret number is " << secretnumber<< endl;
+                    cout <<"\nBetter luck next time!" << endl;
+                    break;
+                }
+            }
+        }
+    }
+    
+    
 }
